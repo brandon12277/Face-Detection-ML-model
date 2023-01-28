a9da14933f5aee2dd0851900bb1c306731ea407e
brandon <williamsbrand879@yahoo.com>
2023-01-28 12:02:47 +0530

    changed database

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 import base64
 import urllib
 import numpy as np
-from pyngrok import ngrok
+# from pyngrok import ngrok
 from sklearn.neighbors import KNeighborsClassifier
 import os
 import cv2
 import joblib
 import json
+import face_recognition
 from json import JSONEncoder
 import numpy
 app = Flask(__name__, static_folder="static")
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///images.db"
-ngrok.set_auth_token("2Kp1iJytvsNhjKisg68c71vFMGx_F7pDwLcMT1PVJnRnjKzp")
-public_url=ngrok.connect(5000).public_url
-print(public_url)
+# ngrok.set_auth_token("2Kp1iJytvsNhjKisg68c71vFMGx_F7pDwLcMT1PVJnRnjKzp")
+# public_url=ngrok.connect(5000).public_url
+# print(public_url)
 db = SQLAlchemy(app)
 
 
@@ -33,7 +34,8 @@ class User(db.Model):
 
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
+known_face_encodings = []
+known_face_names = []
 
 
 
@@ -50,28 +52,47 @@ def gen_image_from_base64(url):
 def gen_frames(frame):
         faces = faceCascade.detectMultiScale(frame, 1.1, 4)
         for (x, y, w, h) in faces:
-            return frame[y:y + h, x:x + w]
+            # return frame[y:y+h,x:x+w]
+            return frame
         return ""
 
 
-def train_model(img_blob_data):
-    faces = []
-    labels = []
-    for user, data in img_blob_data.items():
-        for img in data:
-            resized_face = cv2.resize(img, (50, 50))
-            faces.append(resized_face.ravel())
-            labels.append(user)
-    faces = np.array(faces)
-    knn = KNeighborsClassifier(n_neighbors=4)
-    knn.fit(faces,labels)
-    joblib.dump(knn,'static/face_recognition_model.pkl')
-
-def identify_face(facearray):
-    model = joblib.load('static/face_recognition_model.pkl')
-    return model.predict(facearray)
+def train_model(link):
+    frame = face_recognition.load_image_file(link)
+    face_encoding = face_recognition.face_encodings(frame)[0]
+    return face_encoding
 
 
+
+
+def identify_face(frame):
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+
+        # # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+    return name
 
 
 @app.route("/",methods=["GET","POST"])
@@ -92,9 +113,9 @@ def video():
 @app.route("/results",methods=["GET","POST"])
 def result():
     if request.method=="POST":
+      global known_face_encodings, known_face_names
       img_data=request.form.get("photo_data")
       img_data=img_data.split("*")
-      img_frame_data = []
 
       newusername = request.form.get('username')
       roll = request.form.get('roll')
@@ -103,37 +124,56 @@ def result():
       db.session.commit()
       userid = User.query.filter_by(name=newusername).first().id
 
-      for frame in img_data:
-         img_frame_data.append(frame)
 
-      img_blob=json.dumps(img_frame_data)
+      img_blob=json.dumps(img_data)
       img_rec = Img(id=userid,name=newusername,img_blob=img_blob)
       db.session.add(img_rec)
       db.session.commit()
 
       img_blobs=Img.query.all()
-      img_blob_data={}
       for imgs in img_blobs:
           img_data=json.loads(imgs.img_blob)
-          img_blob_data[imgs.name]=[]
           for frame in img_data:
             frame_2 = gen_image_from_base64(frame)
-            frame_3 = gen_frames(frame_2)
-            if (frame_3 != ""):
-                img_blob_data[imgs.name].append(frame_3)
+            frame_3=gen_frames(frame_2)
+            if frame_3!="":
+                userimagefolder = 'static/faces/' + imgs.name + '_' + str(imgs.id)
+                if not os.path.isdir(userimagefolder):
+                    os.makedirs(userimagefolder)
+                    name=imgs.name+'_'+str(imgs.id)+'.jpg'
+                    cv2.imwrite(userimagefolder + '/' + name, frame_3)
+                break
+      userlist = os.listdir('static/faces')
+      for user in userlist:
+          for imgname in os.listdir(f'static/faces/{user}'):
+              known_face_encodings.append(train_model(f'static/faces/{user}/{imgname}'))
+              known_face_names.append(user)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
       display=""
 
-      train_model(img_blob_data)
+
       return render_template("home.html", notice="registered")
+    return f"<p></p>"
 
 
 
 @app.route("/modeltest",methods=["GET","POST"])
 def modelTesting():
     if request.method=="POST":
+      print(known_face_names)
       img_data=request.form.get("photo_data")
       img_data=img_data.split("*")
       prediction=""
@@ -141,10 +181,7 @@ def modelTesting():
             frame_2 = gen_image_from_base64(frame)
             frame_3 = gen_frames(frame_2)
             if (frame_3 != ""):
-                face = cv2.resize(frame_3, (50, 50))
-                prediction=identify_face(face.reshape(1,-1))
-                # data=User.query.filter_by(name=prediction).first()
-                # display="USER : "+data.name+str(data.roll)
+                prediction=identify_face(frame_3)
                 return render_template("home.html",notice=prediction)
 
     if request.method == "GET":
